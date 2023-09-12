ab05c13b651f177e362602383e30de3441b1b1ef
Chanin Nantasenamat <51851491+dataprofessor@users.noreply.github.com>
2023-09-12 03:54:32 +0000

    Added a balloon emoji.

--- Hello.py
+++ Hello.py
@@ -24,7 +24,7 @@ def run():
         page_icon="👋",
     )
 
-    st.write("# Welcome to Streamlit! 👋")
+    st.write("# Welcome to Streamlit! 🎈")
 
     st.sidebar.success("Select a demo above.")
 
